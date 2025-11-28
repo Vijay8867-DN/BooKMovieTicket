9443f268cd5c127784ec6a4d7200c6e831327faa
vijaydn634 <vijaydn634@gamil.com>
2025-11-28 14:38:15 +0530

    creating impartand packages for project creation

---
+++ src/main/java/com/vijay/book_movie_ticket1/BookMovieTicket1Application.java
@@ -0,0 +1,13 @@
+package com.vijay.book_movie_ticket1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMovieTicket1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMovieTicket1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vijay/book_movie_ticket1/BookMovieTicket1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.book_movie_ticket1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMovieTicket1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
